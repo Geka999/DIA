ccaea67cf356287b232206410e0750ff66e732d6
Qqqqq <namespacesstd@gmail.com>
2023-02-21 15:14:22 +0200

    cardProperty added

--- wwwroot/js/controllers/diaController.js
+++ wwwroot/js/controllers/diaController.js
@@ -2,11 +2,23 @@
 dApp.controller("diaController", function ($scope) {
 
     $scope.class = "";
+    $scope.cardPropertyHeight = {};
+    $scope.cardPropertyHeight.height = 0;
 
     $scope.cardClick = function () {
         if ($scope.class === "")
             $scope.class = "rotate";
         else
             $scope.class = "";
+    };
+
+    $scope.showCardProperty = function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        if ($scope.cardPropertyHeight.height === "300px")
+            $scope.cardPropertyHeight.height = "0px";
+        else
+            $scope.cardPropertyHeight.height = "300px";;
     };
 });
