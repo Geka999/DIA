8ceb2499d14cb1dc940c0acf9a39ee5694366a6f
Qqqqq <namespacesstd@gmail.com>
2023-02-21 23:19:27 +0200

    finished expend card

--- wwwroot/js/controllers/diaController.js
+++ wwwroot/js/controllers/diaController.js
@@ -4,21 +4,28 @@ dApp.controller("diaController", function ($scope) {
     $scope.class = "";
     $scope.cardPropertyHeight = {};
     $scope.cardPropertyHeight.height = 0;
+    $scope.blur = "";
 
     $scope.cardClick = function () {
-        if ($scope.class === "")
+        if ($scope.class === "") {
             $scope.class = "rotate";
-        else
-            $scope.class = "";
+        }
+        else {
+            $scope.class = "";            
+        }
     };
 
     $scope.showCardProperty = function (event) {
         event.preventDefault();
         event.stopPropagation();
 
-        if ($scope.cardPropertyHeight.height === "300px")
+        if ($scope.cardPropertyHeight.height === "300px") {
             $scope.cardPropertyHeight.height = "0px";
-        else
-            $scope.cardPropertyHeight.height = "300px";;
+            $scope.blur = "noBlur";
+        }
+        else {
+            $scope.cardPropertyHeight.height = "300px";
+            $scope.blur = "blur";
+        }
     };
 });
