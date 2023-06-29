038f274c0186dcd3ca869dcc1aaf7b0f03763270
Qqqqq <H2N1@outlook.com>
2023-06-29 11:17:36 +0300

    added card with info

--- wwwroot/js/controllers/diaController.js
+++ wwwroot/js/controllers/diaController.js
@@ -5,6 +5,13 @@ dApp.controller("diaController", function ($scope) {
     $scope.cardPropertyHeight = {};
     $scope.cardPropertyHeight.height = 0;
     $scope.blur = "";
+    $scope.cardPropertyHeight.display = "";
+
+    $scope.class2 = "";
+    $scope.cardPropertyHeight2 = {};
+    $scope.cardPropertyHeight2.height = 0;
+    $scope.cardPropertyHeight2.display = "";
+    $scope.cardPropertyHeight2['z-index'] = "";
 
     $scope.cardClick = function () {
         if ($scope.class === "") {
@@ -22,10 +29,27 @@ dApp.controller("diaController", function ($scope) {
         if ($scope.cardPropertyHeight.height === "300px") {
             $scope.cardPropertyHeight.height = "0px";
             $scope.blur = "noBlur";
+            //$scope.cardPropertyHeight.display = "none"
         }
         else {
             $scope.cardPropertyHeight.height = "300px";
             $scope.blur = "blur";
+            $scope.cardPropertyHeight.display = ""
+        }
+    };
+
+    $scope.showCardProperty2 = function (event) {
+        if ($scope.cardPropertyHeight2.height === "763px") {
+            $scope.cardPropertyHeight2.height = "0px";
+            $scope.cardPropertyHeight.height = "0px";
+            //$scope.cardPropertyHeight.display = "none"
+            //$scope.cardPropertyHeight2.display = "none"
+            $scope.blur = "noBlur";
+        }
+        else {
+            $scope.cardPropertyHeight2.height = "763px";
+            $scope.cardPropertyHeight2.display = "block"
+            $scope.cardPropertyHeight2['z-index'] = "5";
         }
     };
 });
